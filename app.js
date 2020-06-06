d2ef6ff38d4d6fc315bb853a5d04fb386d8aed29
Michal Wozniak <wozniak.michal91@gmail.com>
2020-06-06 15:55:01 +0100

    navbar added

--- app.js
+++ app.js
@@ -5,7 +5,10 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var newsRouter = require('./routes/news');
+var pollRouter = require('./routes/poll');
+var adminRouter = require('./routes/admin');
+
 
 var app = express();
 
@@ -19,16 +22,28 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(function (req, res, next) {
+  // assign req.path to local variable as a "path"
+  res.locals.path = req.path
+  // let routing go on
+  next()
+})
+
+
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/news', newsRouter);
+app.use('/poll', pollRouter);
+app.use('/admin', adminRouter);
+
+
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
--- routes/poll.js
+++ routes/poll.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 /* GET home page. */
 router.get('/', (req, res, next) => {
-  res.render('index', { title: 'Express' });
+    res.render('poll', { title: 'Poll' });
 });
 
 module.exports = router;
